b84c9f38be0c887d8b5b86252238f25f7e3f6b0c
Tosin Moronfolu <tosinmoronfolu@gmail.com>
2022-06-01 13:48:15 +0100

    move auth app out and delete all files

--- yasuke-auth-app/src/controllers/photo.controller.ts
+++
@@ -1,7 +0,0 @@
-import { ImageService } from '../services/image.service';
-import { Controller } from '@nestjs/common';
-
-@Controller('photos')
-export class PhotoController {
-  constructor(private imageService: ImageService) {}
-}
